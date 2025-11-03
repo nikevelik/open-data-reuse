1a1f3d594392c8d7d8eeecd4d6b0d4c782636784
Nikola Georgiev <nikola.georgiev@sap.com>
2025-11-03 16:41:52 +0200

    combine in 1 html

--- app/map.js
+++
@@ -1,4 +0,0 @@
-const Data = {
-    geojson: async() => d3.json("https://raw.githubusercontent.com/yurukov/Bulgaria-geocoding/master/municipalities.geojson"),
-    municip: async() => d3.csv("https://raw.githubusercontent.com/yurukov/Bulgaria-geocoding/master/municipalities.csv")
-};
